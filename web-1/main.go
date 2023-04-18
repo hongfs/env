9f259c09b82af1fb214444a6c8a31b65156891e1
hongfs <hong@hongfs.cn>
2023-04-18 16:51:40 +0800

    #

--- web-1/main.go
+++ web-1/main.go
@@ -3,11 +3,27 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
+
+var Hostname = ""
+
+func init() {
+	Hostname, _ = os.Hostname()
+}
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "1")
+	})
+
+	http.HandleFunc("/hostname", func(w http.ResponseWriter, r *http.Request) {
+		if Hostname == "" {
+			fmt.Fprint(w, "无法获取主机名")
+			return
+		}
+
+		fmt.Fprint(w, Hostname)
 	})
 
 	http.ListenAndServe(":80", nil)
