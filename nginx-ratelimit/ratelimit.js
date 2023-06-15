2fa2c94f76f9c0872524523f166dca184e32d763
hongfs <hong@hongfs.cn>
2023-06-15 17:14:59 +0800

    新增 nginx 限流测试模块

---
+++ nginx-ratelimit/ratelimit.js
@@ -0,0 +1,27 @@
+async function fetch(r) {
+    let headers = {
+        'X-Ratelimit-Host': r.headersIn.host,
+        'X-Ratelimit-Uri': r.uri,
+        'X-Ratelimit-IP': r.remoteAddress,
+    };
+
+    for (h in r.headersIn) {
+        headers['X-Header-' + h] = r.headersIn[h];
+    }
+
+    let result = await ngx.fetch('https://ratelimit.hongfs.cn/', {
+        method: r.method,
+        headers,
+        verify: true,
+    });
+
+    // 200: 可以访问
+    // 429: 限流
+    // 500：服务器错误
+
+    return result.status;
+}
+
+export default {
+    fetch,
+};
