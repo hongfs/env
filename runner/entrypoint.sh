3e514d775046c68d3f704dd235d3eb7519fb4a08
hongfs <hong@hongfs.cn>
2023-12-24 10:37:06 +0800

    Update entrypoint.sh

--- runner/entrypoint.sh
+++ runner/entrypoint.sh
@@ -8,9 +8,9 @@ alias docker="podman"
 
 cd /runner/actions-runner
 
-echo "128" > /proc/sys/sunrpc/tcp_slot_table_entries
-
 if [ ! -z "$NAS_HOST" ]; then
+    echo "128" > /proc/sys/sunrpc/tcp_slot_table_entries
+
     mount -t nfs -o vers=4,minorversion=0,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,_netdev,noresvport $NAS_HOST:/cache/ /mnt/cache/
 fi
 
