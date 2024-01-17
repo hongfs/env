a4ecc2163092859372a1e5c7b3a2eae737d814e1
hongfs <hong@hongfs.cn>
2024-01-17 23:43:22 +0800

    Update update_ghcr_image.sh

--- runner-test/update_ghcr_image.sh
+++ runner-test/update_ghcr_image.sh
@@ -2,21 +2,19 @@
 
 set -e
 
-echo "hongfs----------------------"
+if [ "$HAS_HONGKONG_VPC_REGISTRY" != "1" ]; then
+    exit 0
+fi
 
-pwd
+if [ -e "$(pwd)/Dockerfile" ]; then
+    echo "Dockerfile exists"
 
-echo "hongfs----------------------"
-
-export
-
-echo "hongfs----------------------"
-
-if [ -f "Dockerfile" ]; then
     sed -i "s+ghcr.io/hongfs/env:+registry-vpc.cn-hongkong.aliyuncs.com/hongfs/env:+g" Dockerfile
 fi
 
 if [ -d ".github/workflows/" ]; then
+    echo ".github/workflows/ exists"
+
     sed -i "s+ghcr.io/hongfs/env:+registry-vpc.cn-hongkong.aliyuncs.com/hongfs/env:+g" .github/workflows/*
     sed -i "s+ghcr.io/hongfs/ecs-command-actions:main+registry-vpc.cn-hongkong.aliyuncs.com/hongfs/ecs-command-actions:main+g" .github/workflows/*
 fi
