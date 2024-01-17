f1b5c5f32f48347e7aa7b35e748d5358b0179296
hongfs <hong@hongfs.cn>
2024-01-17 23:58:20 +0800

    Update update_ghcr_image.sh

--- runner-test/update_ghcr_image.sh
+++ runner-test/update_ghcr_image.sh
@@ -2,11 +2,9 @@
 
 set -e
 
-if [ -e "$(pwd)/Dockerfile" ]; then
-    echo "Dockerfile exists"
+dir=$(pwd)
 
-    sed -i "s+ghcr.io/hongfs/env:+registry-vpc.cn-hongkong.aliyuncs.com/hongfs/env:+g" Dockerfile
-fi
+sed -i "s+ghcr.io/hongfs/env:+registry-vpc.cn-hongkong.aliyuncs.com/hongfs/env:+g" $(dir)/Dockerfile
 
 if [ -d ".github/workflows/" ]; then
     echo ".github/workflows/ exists"
