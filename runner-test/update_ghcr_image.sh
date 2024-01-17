9951f7e6f63d7f7e7ebff5d3b4174add249ee458
hongfs <hong@hongfs.cn>
2024-01-17 23:32:04 +0800

    Update update_ghcr_image.sh

--- runner-test/update_ghcr_image.sh
+++ runner-test/update_ghcr_image.sh
@@ -12,3 +12,11 @@ export
 
 echo "hongfs----------------------"
 
+if [ -f "Dockerfile" ]; then
+    sed -i "s+ghcr.io/hongfs/env:+registry-vpc.cn-hongkong.aliyuncs.com/hongfs/env:+g" Dockerfile
+fi
+
+if [ -d ".github/workflows/" ]; then
+    sed -i "s+ghcr.io/hongfs/env:+registry-vpc.cn-hongkong.aliyuncs.com/hongfs/env:+g" .github/workflows/*
+    sed -i "s+ghcr.io/hongfs/ecs-command-actions:main+registry-vpc.cn-hongkong.aliyuncs.com/hongfs/ecs-command-actions:main+g" .github/workflows/*
+fi
